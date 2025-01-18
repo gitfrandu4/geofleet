65c7e7e9d5ae87f891d5822798a46a651027f0dc
Francisco J <franlsym@users.noreply.github.com>
2025-01-18 20:43:12 +0000

    Agregar modelo de datos para la información del vehículo y adaptador de vehículos

---
+++ app/src/main/java/com/example/geofleet/data/model/VehicleInfo.kt
@@ -0,0 +1,10 @@
+package com.example.geofleet.data.model
+
+import com.example.geofleet.data.local.VehiclePositionEntity
+
+data class VehicleInfo(
+        val id: String,
+        val name: String = "",
+        val images: List<String> = emptyList(),
+        val lastPosition: VehiclePositionEntity? = null
+)
